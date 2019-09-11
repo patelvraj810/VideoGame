9a458202ac5638b454707ce2e7bbb7bde14dd62a
Vraj <https://github.com/patelvraj810/Vraj-Patel.git>
2019-09-11 17:28:43 -0400

    Add project files.

---
+++ VideoGame Store/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoGame_Store.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
